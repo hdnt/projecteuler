49b76776a3ea241db90d97b9ee60e7a4764d1077
Hayden T <hhtremper@gmail.com>
2021-01-01 15:33:24 -0800

    Add cpp solution 14

--- cpp_solutions/problem014.cpp
+++ cpp_solutions/problem014.cpp
@@ -1,57 +1,63 @@
 #include <iostream>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 using namespace std;
 
-const int MAX_NUMBER = 5000000;
-unordered_map<int, int> table;
-unordered_map<int, int> ans;
+typedef long long ll;
 
-void solve(int);
-void calc(int);
-int next(int);
+const ll MAX_NUMBER = 5000000;
+ll table[MAX_NUMBER + 1];
+ll ans[MAX_NUMBER + 1];
+
+void init();
+void solve(ll);
+void calc(ll);
+ll next(ll);
 
 int main()
 {
-    table.reserve(MAX_NUMBER);
-    table[1] = ans[1] = 1;
-    ans.reserve(MAX_NUMBER);
+    init();
     solve(MAX_NUMBER);
 
-    int most_terms_until = 1;
-    for (int n = 2; n <= MAX_NUMBER; ++n)
+    ll most_terms_until = 1;
+    for (ll n = 2; n <= MAX_NUMBER; ++n)
     {
         if (table[n] >= table[most_terms_until])
             most_terms_until = n;
         ans[n] = most_terms_until;
     }
 
-    int num_test_cases;
+    ll num_test_cases;
     cin >> num_test_cases;
 
-    for (int _case = 0; _case < num_test_cases; ++_case)
+    for  (ll _case = 0; _case < num_test_cases; ++_case)
     {
-        int target;
+        ll target;
         cin >> target;
 
         cout << ans[target] << endl;
     }
 }
 
-void solve(int target)
+void init()
 {
-    for (int n = 2; n <= target; ++n)
+    for (ll idx = 0; idx < MAX_NUMBER + 1; ++idx)
+        table[idx] = -1;
+    table[1] = 1;
+}
+
+void solve (ll target)
+{
+    for (ll n = 2; n <= target; ++n)
     {
         calc(n);
     }
 }
 
-void calc(int n)
+void calc (ll n)
 {
-    vector<int> termsList;
+    vector <ll> termsList;
 
-    while (table.find(n) == table.end()) // not in table
+    while (n > MAX_NUMBER || table[n] == -1) // not in table
     {
         termsList.push_back(n);
         n = next(n); 
@@ -60,11 +66,11 @@ void calc(int n)
     auto eit = termsList.end();
     for (auto it = termsList.cbegin(); it != eit; ++it)
     {
-        table[*it] = (eit - it) + table[n];
+        if (*it <= MAX_NUMBER)
+            table[*it] = (eit - it) + table[n];
     }
 }
-
-int next(int n)
+ ll next (ll n)
 {
     if (n % 2 == 0)
         return n >> 1;
